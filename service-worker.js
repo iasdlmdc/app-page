fe4d3f2226931f0ddf70c64a7a117b0c3508e682
iasdlmdc <iasdlm.dc@gmail.com>
2024-12-10 13:51:05 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,21 @@
-self.addEventListener('install', function(event) {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('meu-cache').then(function(cache) {
+    caches.open('linktree-cache').then((cache) => {
       return cache.addAll([
         '/',
         '/index.html',
-        '/manifest.json',
-        '/icone.png'
+        '/style.css',
+        '/app.js',
+        '/icon-192x192.png',
+        '/icon-512x512.png',
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
