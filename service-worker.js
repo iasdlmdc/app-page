950120fecf76889975086d900545b10b49648a9a
iasdlmdc <iasdlm.dc@gmail.com>
2024-12-11 15:48:06 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,17 +8,7 @@ self.addEventListener('install', function(event) {
       ]);
     })
   );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
-});
-self.addEventListener('install', function(event) {
-  self.skipWaiting();
+  self.skipWaiting();  // Garantir que o service worker seja ativado imediatamente após a instalação
 });
 
 self.addEventListener('activate', function(event) {
@@ -33,4 +23,25 @@ self.addEventListener('activate', function(event) {
       );
     })
   );
+  self.clients.claim();  // Garantir que o service worker ative-se e controle a página imediatamente
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registrado com sucesso:', registration);
+      })
+      .catch((error) => {
+        console.log('Falha ao registrar o Service Worker:', error);
+      });
+  });
+}
