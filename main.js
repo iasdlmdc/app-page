c5459038e74e403dd9580b6c11ccdeb636645cc9
iasdlmdc <iasdlm.dc@gmail.com>
2024-12-12 13:10:09 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js')
+    .then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }).catch((error) => {
+      console.log('Service Worker registration failed:', error);
+    });
+}
+
 let deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', (e) => {
@@ -20,12 +29,3 @@ window.addEventListener('beforeinstallprompt', (e) => {
     }
   });
 });
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
-    .then((registration) => {
-      console.log('Service Worker registered with scope:', registration.scope);
-    }).catch((error) => {
-      console.log('Service Worker registration failed:', error);
-    });
-}
