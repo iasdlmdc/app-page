b526f187d45340b63191f6b20ead6a97a295a985
iasdlmdc <iasdlm.dc@gmail.com>
2024-12-12 10:10:17 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,30 @@ self.addEventListener('install', (event) => {
   );
 });
 
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker ativado');
+  event.waitUntil(clients.claim());
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
+      // Tenta buscar a resposta do cache, se não encontrar, faz uma busca na rede
+      return cachedResponse || fetch(event.request).then((networkResponse) => {
+        // Se a resposta da rede for válida, armazena ela no cache
+        if (networkResponse && networkResponse.status === 200) {
+          return caches.open('my-cache').then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+        } else {
+          return networkResponse;
+        }
+      });
+    }).catch((error) => {
+      // Caso ocorra algum erro na busca do cache ou da rede, exibe o erro no console
+      console.error('Erro ao buscar o recurso:', error);
+      throw error;
     })
   );
 });
