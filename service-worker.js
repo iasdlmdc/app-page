1e953fe87334c23f811e6781796d2618ff48d979
iasdlmdc <iasdlm.dc@gmail.com>
2024-12-12 07:47:01 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,49 +1,20 @@
-self.addEventListener('install', function(event) {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('static-v1').then(function(cache) {
+    caches.open('my-cache').then((cache) => {
       return cache.addAll([
-        '/app-page/index.html',
+        '/',
+        '/index.html',
         '/app-page/icon.png',
-        // Adicione outras URLs necessárias aqui
-      ]).catch(function(error) {
-        console.error('Falha ao adicionar arquivos à cache:', error);
-      });
+        '/manifest.json'
+      ]);
     })
   );
-  self.skipWaiting(); // Ativar o Service Worker imediatamente após a instalação
 });
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName !== 'static-v1') {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim(); // Garantir que o service worker ative-se e controle a página imediatamente
-});
-
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
     })
   );
 });
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/app-page/service-worker.js')
-      .then((registration) => {
-        console.log('Service Worker registrado com sucesso:', registration);
-      })
-      .catch((error) => {
-        console.log('Falha ao registrar o Service Worker:', error);
-      });
-  });
-}
