6bd421158950a6dd2664ae252d9879ec7863f381
iasdlmdc <iasdlm.dc@gmail.com>
2024-12-13 12:49:03 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,25 @@
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('my-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/app-page/icon.png',
-        '/manifest.json'
-      ]);
-    })
-  );
+    self.skipWaiting(); // Força o 'service worker' a ser ativado imediatamente
+    event.waitUntil(
+        caches.open('site-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json',
+                'https://iasdlmdc.github.io/app-page/icon-512x512.png'
+            ]);
+        })
+    );
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Service Worker ativado');
-  event.waitUntil(clients.claim());
+    event.waitUntil(self.clients.claim()); // Faz com que o service worker atue imediatamente sobre as páginas
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      // Tenta buscar a resposta do cache, se não encontrar, faz uma busca na rede
-      return cachedResponse || fetch(event.request).then((networkResponse) => {
-        // Se a resposta da rede for válida, armazena ela no cache
-        if (networkResponse && networkResponse.status === 200) {
-          return caches.open('my-cache').then((cache) => {
-            cache.put(event.request, networkResponse.clone());
-            return networkResponse;
-          });
-        } else {
-          return networkResponse;
-        }
-      });
-    }).catch((error) => {
-      // Caso ocorra algum erro na busca do cache ou da rede, exibe o erro no console
-      console.error('Erro ao buscar o recurso:', error);
-      throw error;
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
