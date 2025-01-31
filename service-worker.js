03b5d5b763da1ebb2b5d21b065f7f7d554ed6653
iasdlmdc <iasdlm.dc@gmail.com>
2025-01-31 08:42:27 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,21 +3,20 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
-        '/', // Aqui você adiciona a raiz do site (index.html)
-        '/index.html',
-        '/style.css',
-        '/icon-192x192.png',
-        '/icon-512x512.png'
+        '/app-page/',  // Página inicial
+        '/app-page/index.html',
+        '/app-page/style.css',  // Caso tenha algum arquivo CSS
+        '/app-page/icon-192x192.png',
+        '/app-page/icon-512x512.png'
       ]);
     })
   );
 });
 
-// A lógica de fetch pode ser mais simples ou personalizada conforme necessário
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      return response || fetch(event.request); // Se não houver cache, busca a requisição na rede
+      return response || fetch(event.request);
     })
   );
 });
