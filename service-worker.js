b6399cb95ce6132a04c97065caa85539fb79eb80
iasdlmdc <iasdlm.dc@gmail.com>
2025-01-31 08:27:08 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,47 +1,22 @@
-// Armazenar recursos em cache para funcionamento offline
-const CACHE_NAME = 'pwa-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/favicon.ico',
-  '/icon-192x192.png',
-  '/icon-512x512.png',
-  '/manifest.json',
-];
-
-// Instalar o Service Worker e armazenar em cache os recursos necessários
-self.addEventListener('install', (event) => {
+// service-worker.js
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Abrindo cache e armazenando recursos');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// Ativar o Service Worker e limpar caches antigos
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/icon-192x192.png',
+        '/icon-512x512.png'
+      ]);
     })
   );
 });
 
-// Buscar os recursos armazenados em cache ou da rede
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then((cachedResponse) => {
-        return cachedResponse || fetch(event.request);
-      })
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
   );
 });
