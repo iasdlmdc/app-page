36f3aee5c9554b7459de9d65e487f8e08ae74a14
iasdlmdc <iasdlm.dc@gmail.com>
2025-02-17 11:37:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let deferredPrompt;
 if ('serviceWorker' in navigator) {
   const enableServiceWorkerCheckbox = document.getElementById('enableServiceWorker');
   const registerShortcutBtn = document.getElementById('registerShortcutBtn');
+  const captchaCorrect = document.getElementById('captcha-correct');
 
   // Função para registrar o Service Worker
   const registerServiceWorker = () => {
@@ -15,16 +16,16 @@ if ('serviceWorker' in navigator) {
       });
   };
 
-  // Quando o checkbox é marcado, o Service Worker é registrado e o botão aparece
+  // Quando o checkbox é marcado e o captcha for resolvido, o Service Worker é registrado e o botão aparece
   enableServiceWorkerCheckbox.addEventListener('change', (event) => {
-    if (event.target.checked) {
-      // Registra o Service Worker assim que o checkbox for marcado
+    if (event.target.checked && captchaCorrect.checked) {
+      // Registra o Service Worker
       registerServiceWorker();
 
       // Exibe o botão "Adicionar à Tela Inicial"
       registerShortcutBtn.style.display = 'inline-block';
     } else {
-      // Esconde o botão se o checkbox for desmarcado
+      // Esconde o botão se o captcha não foi resolvido
       registerShortcutBtn.style.display = 'none';
     }
   });
@@ -35,22 +36,27 @@ if ('serviceWorker' in navigator) {
     e.preventDefault();
     // Armazena o evento para ser disparado mais tarde
     deferredPrompt = e;
+    console.log("Evento beforeinstallprompt disparado");
 
     // Quando o botão de instalação for clicado
     registerShortcutBtn.addEventListener('click', () => {
       // Exibe o prompt
-      deferredPrompt.prompt();
+      if (deferredPrompt) {
+        deferredPrompt.prompt(); // Solicita o prompt de instalação
 
-      // Quando o usuário escolher se quer ou não instalar
-      deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('Usuário aceitou o prompt de instalação');
-        } else {
-          console.log('Usuário rejeitou o prompt de instalação');
-        }
-        // Após a escolha, descarta o evento
-        deferredPrompt = null;
-      });
+        // Quando o usuário escolher se quer ou não instalar
+        deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('Usuário aceitou o prompt de instalação');
+          } else {
+            console.log('Usuário rejeitou o prompt de instalação');
+          }
+          // Após a escolha, descarta o evento
+          deferredPrompt = null;
+        });
+      } else {
+        console.log('O evento beforeinstallprompt não foi capturado corretamente.');
+      }
     });
   });
 }
