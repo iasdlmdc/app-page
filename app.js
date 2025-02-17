0c6f68bb43702b31cbf8f611f377c6573c3576fe
iasdlmdc <iasdlm.dc@gmail.com>
2025-02-17 14:08:16 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Verifique se o navegador suporta service workers
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+  navigator.serviceWorker.register('app-page/service-worker.js').then(function(registration) {
     console.log('Service Worker registrado com sucesso:', registration);
   }).catch(function(error) {
     console.log('Falha ao registrar o Service Worker:', error);
