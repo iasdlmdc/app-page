11c51e651ea8109730b3bfd075c97c63b4cf6d7f
iasdlmdc <iasdlm.dc@gmail.com>
2025-02-17 14:02:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,42 +5,39 @@ if ('serviceWorker' in navigator) {
   }).catch(function(error) {
     console.log('Falha ao registrar o Service Worker:', error);
   });
+} else {
+  console.log('Navegador não suporta Service Workers');
 }
 
-// Verifique se o navegador suporta a instalação de PWA
 let deferredPrompt;
 const installBtn = document.getElementById('installShortcutBtn');
 const redirectBtn = document.getElementById('redirectToLinktreeBtn');
 const instructions = document.getElementById('installationInstructions');
 const iosInstructions = document.getElementById('iosInstructions');
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Impedir o mini-infobar de aparecer no mobile
-  e.preventDefault();
-  // Salvar o evento para que possa ser acionado posteriormente
-  deferredPrompt = e;
-  // Mostrar o botão de instalação
-  installBtn.style.display = 'block';
-});
+// Exibir o botão de instalação manualmente (para fins de teste)
+installBtn.style.display = 'block';
 
 installBtn.addEventListener('click', (e) => {
-  // Ocultar o botão de instalação
-  installBtn.style.display = 'none';
-  // Mostrar o prompt de instalação
-  deferredPrompt.prompt();
-  // Verificar o que o usuário escolheu
-  deferredPrompt.userChoice.then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('Usuário aceitou a instalação do PWA');
-    } else {
-      console.log('Usuário dispensou a instalação do PWA');
-    }
-    deferredPrompt = null;
-  });
+  console.log('Botão de instalação clicado');
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('Usuário aceitou a instalação do PWA');
+      } else {
+        console.log('Usuário dispensou a instalação do PWA');
+      }
+      deferredPrompt = null;
+    });
+  } else {
+    console.log('deferredPrompt não está disponível');
+  }
 });
 
 // Redireciona para a página do Linktree
 redirectBtn.addEventListener('click', () => {
+  console.log('Botão de redirecionamento clicado');
   window.location.href = 'https://linktr.ee/iasdlm.dc';
 });
 
