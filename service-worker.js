3ee845457e022ef0acd4dc42de5b56aa12d963df
iasdlmdc <iasdlm.dc@gmail.com>
2025-02-17 14:07:52 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,45 @@
-self.addEventListener('install', event => {
+const CACHE_NAME = 'iasd-leonidas-cache-v1';
+const urlsToCache = [
+  '/',
+  '/app-page/index.html',
+  '/app-page/manifest.json',
+  '/app-page/icon-192x192.png',
+  '/app-page/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('my-cache').then(cache => {
-      const filesToCache = [
-        '/app-page/index.html',  // Caminho correto do arquivo HTML
-        '/app-page/app.js'       // Caminho correto do arquivo JS
-      ];
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
 
-      console.log('Adicionando arquivos ao cache: ', filesToCache);
-
-      // Tenta adicionar os arquivos ao cache
-      return cache.addAll(filesToCache)
-        .then(() => {
-          console.log('Arquivos adicionados com sucesso ao cache');
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
         })
-        .catch(error => {
-          console.error('Falha ao adicionar arquivos ao cache:', error);
-        });
+      );
     })
   );
 });
 
-self.addEventListener('activate', event => {
-  console.log('Service Worker ativado!');
-});
-
-self.addEventListener('fetch', event => {
-  // Intercepta as requisições e tenta servir o conteúdo do cache
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
       })
   );
 });
