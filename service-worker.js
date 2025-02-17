d32aeae6cbdc565a592e0192f86ac6a1b5123025
iasdlmdc <iasdlm.dc@gmail.com>
2025-02-17 11:04:16 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,11 @@
-// service-worker.js
 self.addEventListener('install', (event) => {
-  console.log('Service Worker instalado');
+  console.log('Service Worker instalado!');
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Service Worker ativado');
+  console.log('Service Worker ativado!');
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
 });
