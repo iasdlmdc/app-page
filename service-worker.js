edfb485e746d984e58118401cc1c17a2632fc1c1
iasdlmdc <iasdlm.dc@gmail.com>
2025-02-17 12:17:55 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,26 @@
 self.addEventListener('install', (event) => {
   console.log('Service Worker instalado!');
-  
-  // Durante a instalação, vamos cachear os arquivos necessários
+
+  // Durante a instalação, vamos tentar cachear os arquivos necessários
   event.waitUntil(
     caches.open('pwa-cache').then((cache) => {
       return cache.addAll([
         '/',  // Cache da página inicial (se necessário)
-        'https://linktr.ee/iasdlm.dc',  // Cache da URL do Linktree
         'index.html',
         'app.js',
         'manifest.json',
         'icon-192x192.png',
         'icon-512x512.png'
-      ]);
+      ]).catch((error) => {
+        console.error('Falha ao adicionar arquivos ao cache:', error);
+      });
     })
   );
 });
 
 self.addEventListener('activate', (event) => {
   console.log('Service Worker ativado!');
-  
+
   // Vamos ativar imediatamente o SW, removendo os caches antigos (se houver)
   event.waitUntil(
     caches.keys().then((cacheNames) => {
@@ -45,16 +46,18 @@ self.addEventListener('fetch', (event) => {
       if (cachedResponse) {
         return cachedResponse;
       }
+      
       // Caso contrário, faz a requisição à rede
       return fetch(event.request).then((response) => {
-        // Armazena a resposta da rede no cache para futuras requisições
-        if (event.request.url.includes('linktr.ee')) {
-          // Cache apenas a URL do Linktree
+        // Cache da resposta de recursos apenas do seu domínio
+        if (event.request.url.startsWith('https://iasdlmdc.github.io')) {
           caches.open('pwa-cache').then((cache) => {
             cache.put(event.request, response.clone());
           });
         }
         return response;
+      }).catch((error) => {
+        console.error('Falha ao buscar na rede:', error);
       });
     })
   );
