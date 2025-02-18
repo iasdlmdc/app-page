b9c3834c09896878483d69c646b01f85acd9cd7b
iasdlmdc <iasdlm.dc@gmail.com>
2025-02-18 09:16:34 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,28 +2,31 @@ const installPWABtn = document.getElementById('installPWABtn');
 const redirectToLinktreeBtn = document.getElementById('redirectToLinktreeBtn');
 const installationInstructions = document.getElementById('installationInstructions');
 
-// Função para detectar o sistema operacional e exibir as instruções
-const detectOSAndDisplayInstructions = () => {
-  const userAgent = navigator.userAgent.toLowerCase();
+// Função para detectar o dispositivo (Android ou iOS) e exibir as instruções de instalação
+const getDeviceInstructions = () => {
+  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-  if (userAgent.includes('android')) {
-    installationInstructions.innerHTML = 'Para adicionar o atalho à tela inicial no Android:<br>- Abra o menu de opções (os 3 pontos no canto superior direito)<br>- Selecione "Adicionar à tela inicial".';
-  } else if (userAgent.includes('iphone') || userAgent.includes('ipad')) {
-    installationInstructions.innerHTML = 'Para adicionar o atalho à tela inicial no iOS:<br>- Toque no ícone de compartilhamento (quadrado com seta)<br>- Selecione "Adicionar à Tela de Início".';
+  if (/android/i.test(userAgent)) {
+    return 'Para adicionar à sua tela inicial no Android, clique no ícone de três pontos no canto superior direito do navegador e selecione "Adicionar à tela inicial".';
+  } else if (/iPhone|iPad|iPod/i.test(userAgent)) {
+    return 'Para adicionar à sua tela inicial no iOS, clique no botão "Compartilhar" no canto inferior e selecione "Adicionar à Tela de Início".';
   } else {
-    installationInstructions.innerHTML = 'Para adicionar o atalho à tela inicial, siga as instruções do seu dispositivo.';
+    return 'Para adicionar à sua tela inicial, siga as instruções do seu dispositivo.';
   }
 };
 
-// Verifica o sistema operacional e exibe as instruções
-detectOSAndDisplayInstructions();
+// Exibe as instruções de acordo com o dispositivo
+const displayInstallationInstructions = () => {
+  installationInstructions.style.display = 'block'; // Exibe as instruções
+  installationInstructions.innerHTML = getDeviceInstructions();
+};
 
 // Função para redirecionar o usuário para o Linktree
 redirectToLinktreeBtn.addEventListener('click', () => {
   window.location.href = 'https://linktr.ee/iasdlm.dc';  // Redireciona para o Linktree
 });
 
-// Função para exibir o prompt de instalação PWA
+// Função para exibir as instruções após o clique no botão de instalação
 installPWABtn.addEventListener('click', () => {
-  alert('Agora, você pode adicionar o atalho à sua tela inicial utilizando as instruções acima!');
+  displayInstallationInstructions(); // Exibe as instruções de instalação
 });
