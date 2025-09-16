1eab8032ab1ff3bc7d7e0c6a28eb6de3af17a201
iasdlmdc <iasdlm.dc@gmail.com>
2025-09-16 11:10:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function isInStandaloneMode() {
 
 // Registrar o service worker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker-v3.js')
+  navigator.serviceWorker.register('service-worker-v2.js')
     .then((reg) => {
       console.log('[SW] Registrado com sucesso:', reg);
     })
@@ -88,5 +88,6 @@ if ('serviceWorker' in navigator) {
     reg.update();
   });
 }
+
 
 
